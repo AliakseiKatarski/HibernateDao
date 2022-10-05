8dc977dbef440256582370a0240cd712c4b8c4c7
AliakseiKatarski <Grrees94@gmail.com>
2022-10-05 21:18:11 +0300

    add findById to DefaultRepository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,7 @@
 
-import model.entity.*;
-
-import model.repository.*;
-import model.repository.impl.*;
 
 
 public class Main {
     public static void main(String[] args) {
-
-        UserRepository user=new UserRepositoryImpl();
-        User user1=user.findById(1,User.class);
-        System.out.println(user1);
     }
 }
